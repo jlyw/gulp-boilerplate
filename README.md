22ea890795e89d7c4709e2fef45c5bfd608da794
Jean Law Yim Wan <jlyw@MacBook-Pro-de-Jean.local>
2017-04-02 18:57:46 +0200

    Base structure + sass task

---
+++ README.md
@@ -0,0 +1,35 @@
+# Gulp Boilerplate
+
+**In this documentation**
+
+1. Getting Started
+2. File Structure
+3. License
+
+## Getting started
+
+Make sure you have theses intalled on your computer first.
+
+- [Node.js](https://nodejs.org/en/)
+- [Gulp](http://gulpjs.com/) `sudo npm install -g gulp`
+
+## File Structure
+
+Add your files to the appropriate `src` subdirectories. Gulp will process and and compile them into `dist`.
+
+```
+gulp-boilerplate
+|-- dist/
+|		|-- css/
+|				|-- # stylesheets
+|-- src/
+|		|-- sass/
+|				|-- # sass files
+|-- .babelrc
+|-- gulpfile.babel.js
+|-- package.json
+```
+
+## License
+
+The code is available under the [MIT License](https://github.com/jlyw/gulp-boilerplate/blob/master/LICENSE).
---
+++ gulpfile.babel.js
@@ -0,0 +1,68 @@
+/**
+ * Gulp packages
+ */
+
+ // default
+import gulp from 'gulp';
+import sourcemaps from 'gulp-sourcemaps';
+import rename from 'gulp-rename';
+import watch from 'gulp-watch';
+import flatten from 'gulp-flatten';
+
+// Styles
+import cssnano from 'gulp-cssnano';
+import sass from 'gulp-sass';
+import autoprefixer from 'gulp-autoprefixer';
+import csscomb from 'gulp-csscomb';
+import cssbeautify from 'gulp-cssbeautify';
+
+/**
+ * Paths to project folders
+ */
+ const paths = {
+ 	input: 'src/**/*',
+ 	output: 'dist/',
+ 	styles: {
+ 		input: 'src/sass/**/*.{scss,sass}',
+ 		output: 'dist/css/'
+ 	}
+ };
+
+gulp.task('build:styles', () => {
+	return gulp.src(paths.styles.input)
+		// https://github.com/sass/node-sass
+		.pipe(sass({
+			outputStyle: 'expanded',
+			sourceComments: true
+		}))
+		.pipe(flatten())
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(csscomb())
+    .pipe(cssbeautify({indent: '  '}))
+		.pipe(gulp.dest(paths.styles.output))
+		.pipe(sourcemaps.init())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(cssnano())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(paths.styles.output));
+});
+
+gulp.task('compile', 
+	[
+		'build:styles'
+	]
+)
+
+gulp.task('watch', () => {
+	gulp.watch(paths.input, ['default']);
+});
+
+gulp.task('default',
+	[
+		'compile'
+	]
+)
